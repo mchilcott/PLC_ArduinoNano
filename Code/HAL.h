c7cb82af039543cf13b67d29c1f8364dd1f178a2
Matthew Chilcott <mchilcott@users.noreply.github.com>
2023-12-10 08:54:26 +1300

    As manufactured for v1.0

---
+++ Code/HAL.h
@@ -0,0 +1,24 @@
+// Pin relabelling
+
+const int X0 = 2;
+const int X1 = 3;
+const int X2 = 4;
+const int X3 = 5;
+const int X4 = 12;
+const int X5 = 7;
+const int X6 = 8
+const int X7 = A7;
+
+
+
+const int Y00 = Y0 = 6;
+const int Y01 = Y1 = 9;
+const int Y02 = Y2 = 10;
+const int Y03 = Y2 = 11;
+const int Y04 = Y2 = A6;
+const int Y05 = Y2 = A3;
+const int Y06 = Y2 = A2;
+const int Y07 = Y2 = A1;
+
+const int Y10 = 13;
+const int Y11 = A0;
